dbfd98323922f8fe58bb9d702886b29280311f15
Dominic Michaud <dmichaud@fungo.ca>
2021-04-22 15:50:21 -0400

    fix css / fix validation issue

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 
 //Global variables
 $globalVars = {
-  next_index: 0,
-  current_index: 0,
-  prev_index: 0,
-  current_percent: 0,
-  slider: "",
-  validate_class: '.carousel-item.active.validate-',
-  pass_valid: true,
-  errorHandler: $('.state_handler'),
-  errorContainer: $('.state_handler .messages'),
-  errorMessage: $('.state_handler .messages p')
+    next_index: 0,
+    current_index: 0,
+    prev_index: 0,
+    current_percent: 0,
+    slider: "",
+    validate_class: '.carousel-item.active.validate-',
+    pass_valid: true,
+    errorHandler: $('.state_handler'),
+    errorContainer: $('.state_handler .messages'),
+    errorMessage: $('.state_handler .messages p')
 };
 
-$(function(){
-	//If PC, remove Antialiasing for Fonts
-	if (navigator.userAgent.indexOf('Mac OS X') == -1) {
-	  $("body").removeClass("antialiasing");
-	}
+$(function () {
+    //If PC, remove Antialiasing for Fonts
+    if (navigator.userAgent.indexOf('Mac OS X') == -1) {
+        $("body").removeClass("antialiasing");
+    }
 
     //Masked input init
     $('.phone-mask').mask('(000) 000-0000');
@@ -26,12 +26,12 @@ $(function(){
     //Datepicker init    
     var year_range = "1960:" + new Date().getFullYear().toString();
     console.log(year_range);
-    $( "#user_birth" ).datepicker({
+    $("#user_birth").datepicker({
         changeMonth: true,
         changeYear: true,
         dateFormat: "dd/mm/yy",
         yearRange: year_range
-    });    
+    });
 
     //Multiselect init
     var selected_objectives = '';
@@ -39,38 +39,38 @@ $(function(){
         buttonClass: 'btn btn-info form-control multi-objective',
         buttonContainer: '<div class="btn-group" />',
         buttonWidth: '100%',
-        buttonText: function(options, select) {
+        buttonText: function (options, select) {
             if (options.length === 0) {
                 return 'Aucune option de sélectionnée ...';
             }
             else {
                 return options.length + ' sélectionnés';
-            }                        
+            }
         },
         selectedClass: 'active',
-        onDropdownHidden: function(event) {
+        onDropdownHidden: function (event) {
             selected_objectives = $('button.multi-objective').attr('title');
-            $('#which_objective_hidden').val(selected_objectives);            
+            $('#which_objective_hidden').val(selected_objectives);
         }
     });
- 
+
     var selected_physical = '';
     $('#physical_championship').multiselect({
         buttonClass: 'btn btn-info form-control multi-physical',
         buttonContainer: '<div class="btn-group" />',
         buttonWidth: '100%',
-        buttonText: function(options, select) {
+        buttonText: function (options, select) {
             if (options.length === 0) {
                 return 'Aucune option de sélectionnée ...';
             }
             else {
                 return options.length + ' sélectionnés';
-            }                        
+            }
         },
         selectedClass: 'active',
-        onDropdownHidden: function(event) {
+        onDropdownHidden: function (event) {
             selected_physical = $('button.multi-physical').attr('title');
-            $('#physical_championship_hidden').val(selected_physical);            
+            $('#physical_championship_hidden').val(selected_physical);
         }
     });
 
@@ -79,21 +79,21 @@ $(function(){
         buttonClass: 'btn btn-info form-control multi-training',
         buttonContainer: '<div class="btn-group" />',
         buttonWidth: '100%',
-        buttonText: function(options, select) {
+        buttonText: function (options, select) {
             if (options.length === 0) {
                 return 'Aucune option de sélectionnée ...';
             }
             else {
                 return options.length + ' sélectionnés';
-            }                        
+            }
         },
         selectedClass: 'active',
-        onDropdownHidden: function(event) {
+        onDropdownHidden: function (event) {
             selected_traning = $('button.multi-training').attr('title');
-            $('#training_which_hidden').val(selected_traning);            
+            $('#training_which_hidden').val(selected_traning);
         }
     });
-    
+
     //Form section slider init
     $globalVars.slider = $(".carousel").carousel({
         interval: false,
@@ -103,56 +103,54 @@ $(function(){
     });
 
     //Slider next prev events
-    $(".nav-next").on("click", function() {
+    $(".nav-next").on("click", function () {
         //Validate sections with required fields
-        if(validate_section($globalVars.current_index)){
+        if (validate_section($globalVars.current_index)) {
             $globalVars.slider.carousel("next");
         }
     });
 
-    $(".nav-prev").on("click", function() {
+    $(".nav-prev").on("click", function () {
         $globalVars.slider.carousel("prev");
     });
 
     //Slider: after slided transition event
-    $(".carousel").on("slide.bs.carousel", function(evt) {
+    $(".carousel").on("slide.bs.carousel", function (evt) {
         //Call progress bar update function
         $globalVars.current_index = evt.to;
         if (evt.direction == "left") {
-          $globalVars.prev_index = evt.from;
-          $globalVars.next_index = evt.to;          
-          updateProgress(parseInt($globalVars.next_index));
+            $globalVars.prev_index = evt.from;
+            $globalVars.next_index = evt.to;
+            updateProgress(parseInt($globalVars.next_index));
         } else {
-          $globalVars.prev_index = evt.to;
-          $globalVars.next_index = evt.from;
-          updateProgress(parseInt($globalVars.prev_index));
+            $globalVars.prev_index = evt.to;
+            $globalVars.next_index = evt.from;
+            updateProgress(parseInt($globalVars.prev_index));
         }
     });
-// onchange for weight
-    $('select#weightOption').on('change', function(){
-        var weightValue= $("input#user_weight").val()
-        weightConverter(weightValue)
 
-    })
-    
-    $('input#user_weight').on('change', function(){
-        var weightValue= $("input#user_weight").val()
-        weightConverter(weightValue)
+    // onchange for weight
+    $('select#weightOption').on('change', function () {
+        var weightValue = $("input#user_weight").val();
+        weightConverter(weightValue);
+    });
 
-    })
+    $('input#user_weight').on('change', function () {
+        var weightValue = $("input#user_weight").val();
+        weightConverter(weightValue);
+    });
 
     // onchange for height
-    $('select#heightOption').on('change', function(){
-        var heightValue= $("input#user_height").val()
-        heightConverter(heightValue)
+    $('select#heightOption').on('change', function () {
+        var heightValue = $("input#user_height").val();
+        heightConverter(heightValue);
+    });
 
-    })
-    
-    $('input#user_height').on('change', function(){
-        var heightValue= $("input#user_height").val()
-        heightConverter(heightValue)
+    $('input#user_height').on('change', function () {
+        var heightValue = $("input#user_height").val();
+        heightConverter(heightValue);
+    });
 
-    })
     //Window resize event
     window.onresize = myResize;
     myResize();
@@ -160,38 +158,37 @@ $(function(){
 
 
 //Browser resize event
-function myResize(){
+function myResize() {
     var bWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || window.width();
     var bHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || window.height();
-
 }
 
 //Update progress bar after each slide transition
 function updateProgress(nextIndex) {
-  //Check to see if we came back to the first step
-  if (nextIndex == 0) {
-    //If true, we set our progress to 0%
-    $globalVars.current_percent = 0;
-  } else {
-    //Else we update the progress bar
-    $globalVars.current_percent = 10 * nextIndex;
-  }
+    //Check to see if we came back to the first step
+    if (nextIndex == 0) {
+        //If true, we set our progress to 0%
+        $globalVars.current_percent = 0;
+    } else {
+        //Else we update the progress bar
+        $globalVars.current_percent = 10 * nextIndex;
+    }
 
-  //Update progress bar width property
-  $(".progress-bar").css({
-    width: $globalVars.current_percent + "%"
-  });
+    //Update progress bar width property
+    $(".progress-bar").css({
+        width: $globalVars.current_percent + "%"
+    });
 }
 
 //Validate required fields and display error message to user
-function validate_section(index){
+function validate_section(index) {
     //We make sure the current section exists and has the validate class
-    if($($globalVars.validate_class + index).length > 0){
+    if ($($globalVars.validate_class + index).length > 0) {
         $globalVars.pass_valid = true;
         //Loop each field with the required class
-        $($globalVars.validate_class + index + ' .required').each(function(){
+        $($globalVars.validate_class + index + ' .required').each(function () {
             //if one of them don't have the valid class, we warn the user and pinpoint the error
-            if(!$(this).hasClass('valid')){
+            if (!$(this).hasClass('valid')) {
                 $(this).addClass('invalid');
                 handleResponse('Veuillez remplir les champs obligatoires (*)', 'bg-danger');
                 $globalVars.pass_valid = false;
@@ -200,23 +197,23 @@ function validate_section(index){
         });
 
         //The last section is different, we need to make sure that at least one of the checkboxes is checked
-        if(index === 9){
-            if($($globalVars.validate_class + index + ' input[type=checkbox]').is(':checked')){
+        if (index === 9) {
+            if ($($globalVars.validate_class + index + ' input[type=checkbox]').is(':checked')) {
                 return true;
             }
-            else{
+            else {
                 handleResponse("Veuillez cocher au moins l'une des options", 'bg-danger');
                 $globalVars.pass_valid = false;
                 return false;
             }
         }
-        
+
         //if no errors and valid, the slider advances without problem
-        if($globalVars.pass_valid){
+        if ($globalVars.pass_valid) {
             return true;
         }
     }
-    else{
+    else {
         //if the current section index doesn't need to be validated, the slider advances without problem
         return true;
     }
@@ -233,35 +230,27 @@ function handleResponse(msg, state) {
 
         setTimeout(function () {
             $globalVars.errorContainer.removeClass(state);
-            $globalVars.errorMessage.html('');            
+            $globalVars.errorMessage.html('');
         }, 500)
     }, 3000);
 }
 
 function weightConverter(valNum) {
     var numOption = document.getElementById("weightOption").value;
-    
-    if( numOption=== "lbs"){
-        $("#outputKilograms").html((parseInt(valNum)/2.2046).toFixed(3));
-        
-    }
 
-    else if(numOption === "kg") {
-       $("#outputKilograms").html(parseInt(valNum))
+    if (numOption === "lbs") {
+        $("#outputKilograms").html((parseInt(valNum) / 2.2046).toFixed(3));
+    } else if (numOption === "kg") {
+        $("#outputKilograms").html(parseInt(valNum))
     }
 }
 
 function heightConverter(valNum) {
     var height = document.getElementById("heightOption").value;
-    console.log(height)
-    if( height=== "metre"){
-       $("#meterOutput").html(valNum)
-    }
 
-    else if(height === "pied") {
-        $("#meterOutput").html((parseInt(valNum)/3.2808).toFixed(3));
+    if (height === "metre") {
+        $("#meterOutput").html(valNum)
+    } else if (height === "pied") {
+        $("#meterOutput").html((parseInt(valNum) / 3.2808).toFixed(3));
     }
 }
-    
- 
-  
